336b612bef0e6578c7544162dd03deac93f306a8
miftahilmi15-blip <miftahilmi15@gmail.com>
2025-11-25 13:02:37 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,9 @@
-// firebase.js - initialized with user's config
-// Replace these values only if you change project
-var firebaseConfig = {
-  apiKey: "AIzaSyA9or0xsb5RB-Z-JzntgFEGb51wK0b4w4Y",
-  authDomain: "database-santri-e1240.firebaseapp.com",
-  projectId: "database-santri-e1240",
-  storageBucket: "database-santri-e1240.firebasestorage.app",
-  messagingSenderId: "116390382568",
-  appId: "1:116390382568:web:23ce25e5e3ba3e9087b25d",
-  measurementId: "G-CPKGG6X5TD"
+const firebaseConfig = {
+  apiKey: "AIzaSyD9....",
+  authDomain: "e-santri-123.firebaseapp.com",
+  projectId: "e-santri-123",
+  storageBucket: "e-santri-123.appspot.com",
+  messagingSenderId: "10928291...",
+  appId: "1:10928:web:8ab..."
 };
-// Initialize Firebase (compat)
 firebase.initializeApp(firebaseConfig);
-var auth = firebase.auth();
-var db = firebase.firestore();
