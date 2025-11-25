20f97571d8cb3af63efc33f369599cbd4b100a82
miftahilmi15-blip <miftahilmi15@gmail.com>
2025-11-25 14:23:41 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQ1A9rtEoEYoriA5Y7oSB84Fd1Xnu6aes",
+  authDomain: "e-santri-ed555.firebaseapp.com",
+  projectId: "e-santri-ed555",
+  storageBucket: "e-santri-ed555.firebasestorage.app",
+  messagingSenderId: "588687778472",
+  appId: "1:588687778472:web:3000ae9b18f1004ed2189d",
+  measurementId: "G-MB8M1EGFJS"
+};
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
