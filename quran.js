269b9292b35a4d6e7c51b93a9373f325d5c22c86
miftahilmi15-blip <miftahilmi15@gmail.com>
2026-01-10 22:54:02 +0700

    Update quran.js

--- quran.js
+++ quran.js
@@ -1,12 +1,11 @@
 const surahContent = document.getElementById("surahContent");
 const pageNumberEl = document.getElementById("pageNumber");
 
-// 🔹 List URL SVG (buat Python sudah generate 604 URL)
 const svgUrls = [
   "https://raw.githubusercontent.com/miftahilmi15-blip/miftahilmi15-blip/main/sgv/001.svg",
   "https://raw.githubusercontent.com/miftahilmi15-blip/miftahilmi15-blip/main/sgv/002.svg",
   "https://raw.githubusercontent.com/miftahilmi15-blip/miftahilmi15-blip/main/sgv/003.svg",
-  // ... sampai 604
+  // ... lanjut sampai 604
 ];
 
 let currentPage = 0;
@@ -14,27 +13,33 @@ let currentPage = 0;
 function showPage(page){
   fetch(svgUrls[page])
     .then(res => {
-        if(!res.ok) throw new Error("404 SVG");
-        return res.text();
+      if(!res.ok) throw new Error("404 SVG");
+      return res.text();
     })
     .then(svg => {
-        surahContent.innerHTML = svg;
-        pageNumberEl.textContent = page + 1;
+      surahContent.innerHTML = svg;
+      pageNumberEl.textContent = `${page + 1} / ${svgUrls.length}`;
     })
-    .catch(e => surahContent.innerHTML = "<p>Gagal load halaman SVG</p>");
+    .catch(e => {
+      surahContent.innerHTML = "<p>Gagal load halaman SVG</p>";
+      console.error(e);
+    });
 }
 
 function nextPage(){
-  if(currentPage < svgUrls.length - 1) currentPage++;
-  showPage(currentPage);
+  if(currentPage < svgUrls.length - 1){
+    currentPage++;
+    showPage(currentPage);
+  }
 }
 
 function prevPage(){
-  if(currentPage > 0) currentPage--;
-  showPage(currentPage);
+  if(currentPage > 0){
+    currentPage--;
+    showPage(currentPage);
+  }
 }
 
-// Mode malam
 function toggleDark(){
   document.body.classList.toggle("dark");
 }
