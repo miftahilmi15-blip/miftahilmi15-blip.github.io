b938b079883d1d767432e8f81364cb277efc07ab
miftahilmi15-blip <miftahilmi15@gmail.com>
2026-01-11 14:20:55 +0700

    Update quran.js

--- quran.js
+++ quran.js
@@ -1,37 +1,42 @@
-const surahSelect = document.getElementById("surahSelect");
-const content = document.getElementById("content");
+document.addEventListener("DOMContentLoaded", function () {
 
-// daftar surah
-fetch("https://api.quran.gading.dev/surah")
-  .then(res => res.json())
-  .then(data => {
-    data.data.forEach(surah => {
-      const opt = document.createElement("option");
-      opt.value = surah.number;
-      opt.textContent = surah.number + ". " + surah.name.transliteration.id;
-      surahSelect.appendChild(opt);
-    });
-    loadSurah(1);
-  });
+  const surahSelect = document.getElementById("surahSelect");
+  const content = document.getElementById("content");
 
-surahSelect.addEventListener("change", e => {
-  loadSurah(e.target.value);
-});
-
-function loadSurah(no) {
-  content.innerHTML = "Memuat...";
-  fetch(`https://api.quran.gading.dev/surah/${no}`)
+  fetch("https://api.quran.gading.dev/surah")
     .then(res => res.json())
     .then(data => {
-      content.innerHTML = "";
-      data.data.verses.forEach(v => {
-        const div = document.createElement("div");
-        div.className = "ayah";
-        div.innerHTML = `
-          <div class="arab">${v.text.arab}</div>
-          <div class="arti">${v.translation.id}</div>
-        `;
-        content.appendChild(div);
+      data.data.forEach(surah => {
+        const opt = document.createElement("option");
+        opt.value = surah.number;
+        opt.textContent =
+          surah.number + ". " + surah.name.transliteration.id;
+        surahSelect.appendChild(opt);
       });
+      loadSurah(1);
     });
-}
+
+  surahSelect.addEventListener("change", e => {
+    loadSurah(e.target.value);
+  });
+
+  function loadSurah(no) {
+    content.innerHTML = "Memuat...";
+    fetch(`https://api.quran.gading.dev/surah/${no}`)
+      .then(res => res.json())
+      .then(data => {
+        content.innerHTML = "";
+        data.data.verses.forEach(v => {
+          const div = document.createElement("div");
+          div.className = "ayah";
+          div.innerHTML = `
+            <div class="arab">${v.text.arab}</div>
+            <div class="arti">${v.translation.id}</div>
+          `;
+          content.appendChild(div);
+        });
+      });
+  }
+
+});
+
