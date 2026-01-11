d1560a1b6221f3e606dd65373da5398e6e5437dc
miftahilmi15-blip <miftahilmi15@gmail.com>
2026-01-11 12:35:46 +0700

    Update quran.js

--- quran.js
+++ quran.js
@@ -1,32 +1,35 @@
 const surahContent = document.getElementById("surahContent");
 const pageNumberEl = document.getElementById("pageNumber");
 
-const svgUrls = [
-  "https://raw.githubusercontent.com/miftahilmi15-blip/miftahilmi15-blip.github.io/main/svg/034.svg"
-];
+let currentPage = 1;
+const TOTAL = 604;
 
-let currentPage = 0;
-
-function showPage(page) {
-  fetch(svgUrls[page])
-    .then(res => {
-      if (!res.ok) throw new Error("404 SVG");
-      return res.text();
-    })
-    .then(svg => {
-      surahContent.innerHTML = svg;
-      pageNumberEl.textContent = `${page + 1} / ${svgUrls.length}`;
-    })
-    .catch(e => {
-      surahContent.innerHTML = "<p>Gagal load halaman SVG</p>";
-      console.error(e);
-    });
+function showPage(page){
+  surahContent.innerHTML = `
+    <object 
+      type="image/svg+xml" 
+      data="svg/${String(page).padStart(3,'0')}.svg" 
+      width="100%">
+    </object>
+  `;
+  pageNumberEl.textContent = page;
 }
 
-// Tes tombol navigasi (meskipun hanya 1 halaman)
-function nextPage() {}
-function prevPage() {}
-function toggleDark() {
+function nextPage(){
+  if(currentPage < TOTAL){
+    currentPage++;
+    showPage(currentPage);
+  }
+}
+
+function prevPage(){
+  if(currentPage > 1){
+    currentPage--;
+    showPage(currentPage);
+  }
+}
+
+function toggleDark(){
   document.body.classList.toggle("dark");
 }
 
